1fe13e276b13eec4dbc85740721e723cae011b2b
hadiul islam <hadiulislamofficial@gmail.com>
2021-01-26 20:00:44 +0600

    add

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//login btn handaler
 let enter = document.getElementById('login');
 enter.addEventListener('click', function(){
     const loginAria = document.getElementById('login-aria');
@@ -5,3 +6,34 @@ enter.addEventListener('click', function(){
     const transactionAria = document.getElementById('transaction-aria');
     transactionAria.style.display = "block";
 })
+//deposit btn handaler
+const depositBtn = document.getElementById('addDeposit');
+depositBtn.addEventListener('click',function(){
+    const depositAmaount = document.getElementById('depositAmaount').value;
+    const dipositNumber =parseFloat(depositAmaount);
+    
+    updateSpanText("currentDeposit", dipositNumber)
+    updateSpanText("currentBalance", dipositNumber)
+
+    document.getElementById('depositAmaount').value = "";
+
+})
+
+//withdraw btn handaler
+const withdrawBtn = document.getElementById('addWithdraw');
+withdrawBtn.addEventListener('click',function(){
+    const withdrawAmaount = document.getElementById('withdrawAmount').value;
+    const withdrawNumber =parseFloat(withdrawAmaount);
+
+    updateSpanText("currentWithdraw", withdrawNumber)
+    updateSpanText("currentBalance", -1*withdrawNumber)
+
+    document.getElementById('withdrawAmount').value = "";
+})
+
+function updateSpanText (id, dipositNumber){
+    const presentBalance = document.getElementById(id).innerText;
+    const currentBalanceNumber =parseFloat(presentBalance);
+    const totalBalance = dipositNumber + currentBalanceNumber;
+    document.getElementById(id).innerText= totalBalance;
+}
